eeed0201c2c952ce23f7fc7cde5d35fba678721f
GauravJanoti <janoti.gaurav@gmail.com>
2019-05-10 21:24:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-9mtof3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-9mtof3)
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatButtonToggleChange} from '@angular/material';
+
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+}
